f994ee8631e887d21adb9ad0b50b15f31e7c4145
ssamkkim <ssamuel.kkim@gmail.com>
2023-01-22 18:42:48 -0800

    added specified icons next to transactions

--- src/components/Transaction.tsx
+++ src/components/Transaction.tsx
@@ -2,14 +2,20 @@ import React from 'react';
 import { changeDescriptionFormat } from '../utils/changeDescriptionFormat';
 
 import { getRelativeTime } from '../utils/getRelativeTime';
+import { getTransactionIcon } from '../utils/getTransactionIcon';
 
 const Transaction = ({ transaction, address }: any) => {
   console.log(transaction);
   return (
     <div className="flex justify-between font-bold text-gray-800 py-3 border-b border-gray-300">
-      <div className="flex flex-col py-2 pr-3">
-        <div>{`${changeDescriptionFormat(transaction, address)}`}</div>
-        <div className="text-xs font-normal">{`${transaction.type}`}</div>
+      <div className="flex items-center">
+        <div className="mr-3 text-2xl text-center">
+          {getTransactionIcon(transaction.type)}
+        </div>
+        <div className="flex flex-col py-2 pr-3">
+          <div>{`${changeDescriptionFormat(transaction, address)}`}</div>
+          <div className="text-xs font-normal">{`${transaction.type}`}</div>
+        </div>
       </div>
       <div className="text-xs tracking-tight text-right ml-4">{`${getRelativeTime(
         transaction.timestamp
---
+++ src/utils/getTransactionIcon.tsx
@@ -0,0 +1,46 @@
+import { AiFillFire } from 'react-icons/ai';
+import {
+  BiDollar,
+  BiListPlus,
+  BiQuestionMark,
+  BiTransfer,
+} from 'react-icons/bi';
+import { FaBurn, FaHandHolding, FaHandshake } from 'react-icons/fa';
+import { GiCancel, GiOpenPalm } from 'react-icons/gi';
+import { IoSwapVerticalOutline } from 'react-icons/io5';
+import { MdOutlineSwapVert } from 'react-icons/md';
+import { SiAcclaim } from 'react-icons/si';
+import { TbCandy } from 'react-icons/tb';
+
+export const getTransactionIcon = (txType: string) => {
+  switch (txType) {
+    case 'SWAP':
+      return <IoSwapVerticalOutline />;
+    case 'TRANSFER':
+      return <BiTransfer />;
+    case 'UNKNOWN':
+      return <BiQuestionMark />;
+    case 'NFT_MINT':
+      return <SiAcclaim />;
+    case 'TOKEN_MINT':
+      return <SiAcclaim />;
+    case 'NFT_SALE':
+      return <BiDollar />;
+    case 'NFT_LISTING':
+      return <BiListPlus />;
+    case 'NFT_CANCEL_LISTING':
+      return <GiCancel />;
+    case 'BURN':
+      return <AiFillFire />;
+    case 'BURN_NFT':
+      return <AiFillFire />;
+    case 'CANDY_MACHINE_ROUTE':
+      return <TbCandy />;
+    case 'BORROW_FOX':
+      return <GiOpenPalm />;
+    case 'LOAN_FOX':
+      return <FaHandshake />;
+    default:
+      return <BiQuestionMark />;
+  }
+};
